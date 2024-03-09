52ae9e8cf3bfeff3b0411d56f912d2ead6e4c28d
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-03-09 17:50:02 +0100

    update heade

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,16 +9,12 @@ export default function Header() {
     useEffect(() => {
         fetch('https://mern-blog-backend-hmpo.onrender.com/profile', {
             credentials: 'include',
-        })
-    }, []);
-    //     fetch('https://mern-blog-backend-hmpo.onrender.com/profile', {
-    //         credentials: 'include',
-    //     }).then((response) => {
-    //         response.json().then((userInfo) => {
-    //             setUserInfo(userInfo);
-    //         });
-    //     });
-    // }, [setUserInfo]);
+        }).then((response) => {
+            response.json().then((userInfo) => {
+                setUserInfo(userInfo);
+            });
+        });
+    }, [setUserInfo]);
 
     function logout() {
         fetch('https://mern-blog-backend-hmpo.onrender.com/logout', {
